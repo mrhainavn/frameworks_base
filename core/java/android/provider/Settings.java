036f9e2deae801827ded6ec841c90b5f9875eb14
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    FWB-XuiMod: Toast Animations [1/2]
    @neobuddy89: Fix multiuser usage.
    
    From: westcripp <altan_koruk@hotmail.com>
    Date: Fri, 23 Dec 2016 22:45:27 +0300
    Subject: [PATCH] Frameworks: Re-adapt toast window animations for 7.1.1
    
     Since google updated toast popup notifications on 7.1.1 release. toast window
     animations stop working. So now this is make it work again
    
    Change-Id: Ia88bbf886381bb17b7fa20f7e570aea376955a76
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: mrhainavn <mr.hai.na.vn@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5884,6 +5884,13 @@ public final class Settings {
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
         /**
+         * Toast animations
+         *
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
