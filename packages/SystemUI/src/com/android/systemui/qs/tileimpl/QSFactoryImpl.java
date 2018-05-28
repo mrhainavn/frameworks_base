d3bd1dbb95aec0eb997651b033e1f39b896119d3
ZeNiXxX <vitjahermann@gmail.com>
2018-05-28 21:46:30 +0200

    SystemUI: Add Calendar tile
    Added to pie by @ZeNiXxX

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.CallTile;
 import com.android.systemui.qs.tiles.AlarmTile;
+import com.android.systemui.qs.tiles.CalendarTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.FontTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
@@ -185,6 +186,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new AlarmTile(mHost);
             case "calc":
                 return new CalcTile(mHost);
+            case "calendar":
+                return new CalendarTile(mHost);
         }
 
         // Intent tiles.
