028de5b078d6d3abca725b7067eb5a36740b70a2
ZeNiXxX <vitjahermann@gmail.com>
2018-08-23 09:38:42 +0200

    SystemUI:  Add Alarm tile
    Added to pie by @ZeNiXxX

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -38,6 +38,7 @@ import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.CallTile;
+import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.FontTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
@@ -179,6 +180,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CallTile(mHost);
             case "camera":
                 return new CameraTile(mHost);
+            case "alarm":
+                return new AlarmTile(mHost);
         }
 
         // Intent tiles.
