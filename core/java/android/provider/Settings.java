6a45cdcb468c5c07ba46598305a3a6a346b7c505
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-16 02:29:08 +0700

    Carrier Label styles font & size [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5822,6 +5822,18 @@ public final class Settings {
         private static final Validator ONE_HAND_MODE_ENABLED_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Settings for carrier label font size
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_SIZE = "status_bar_carrier_font_size";
+
+        /**
+         * Custom carrier label font style
+         * @hide
+         */
+        public static final String STATUS_BAR_CARRIER_FONT_STYLE = "status_bar_carrier_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
