a61f01fecb57ac752d9b01869cbee0ce45e9627a
xyyx <xyyx@mail.ru>
2018-10-01 17:20:51 -0600

    Ability to disable dashboard suggestions and conditions (1/3)
    Change-Id: Ic1852ab4770b6bb0aa44032eb4479b99d1683b3e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5433,6 +5433,18 @@ public final class Settings {
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
+
+        /**
           * Volume keys control cursor in text fields (default is 0)
           * 0 - Disabled
           * 1 - Volume up/down moves cursor left/right
