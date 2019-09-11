07a856c0547a1bcd95a288b78c537b21454fe4a5
mrhainavn <mr.hai.na.vn@gmail.com>
2019-09-11 21:33:45 +0700

    Revert "When data switches during voice call, make sure signal bar updates it."
    This reverts commit 1bda9f5b2b44bbf9a6859dab97e18a34a5152b13.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -592,7 +592,7 @@ public class MobileSignalController extends SignalController<
     }
 
     private boolean isDataDisabled() {
-        return !mPhone.isDataCapable();
+        return !mPhone.getDataEnabled(mSubscriptionInfo.getSubscriptionId());
     }
 
     @VisibleForTesting
@@ -612,7 +612,6 @@ public class MobileSignalController extends SignalController<
         pw.println("  mSignalStrength=" + mSignalStrength + ",");
         pw.println("  mDataState=" + mDataState + ",");
         pw.println("  mDataNetType=" + mDataNetType + ",");
-        pw.println("  isDataDisabled=" + isDataDisabled() + ",");
     }
 
     class MobilePhoneStateListener extends PhoneStateListener {
